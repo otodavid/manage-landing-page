dc0d193b04ed36ec0fd48cc4629f1af15d6880f9
David <dieavid01@gmail.com>
2020-07-07 11:24:25 +0100

    desktop view

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-var swiper = new Swiper('.swiper-container', {
+const mobileSwipper = function () {
+  var swiper = new Swiper('.swiper-container', {
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
@@ -7,3 +8,44 @@ var swiper = new Swiper('.swiper-container', {
       },
     },
   });
+}
+
+const desktopSwipper = function () {
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2.5,
+    spaceBetween:30,
+    freeMode: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+}
+
+window.addEventListener('load', () => {
+  const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+  console.log(width)
+  if(width < 1000) {
+    MainNav.classList.add('hide');
+    mobileSwipper();
+  } else {
+    desktopSwipper();
+  }
+})
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const MainNav = document.querySelector('#main-nav');
+
+mobileMenu.addEventListener('click', () => {
+  if (MainNav.classList.contains('hide')) {
+    MainNav.classList.add('show');
+    MainNav.classList.remove('hide');
+    mobileMenu.firstElementChild.src = './images/icon-close.svg';
+  } else {
+    MainNav.classList.remove('show');
+    MainNav.classList.add('hide');
+    mobileMenu.firstElementChild.src = './images/icon-hamburger.svg';
+  }
+
+
+})
